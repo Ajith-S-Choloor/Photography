884cbeb3a2520e8f069772a56e567cfe6a33260a
ananthunalloor <ananthunalloor@gmail.com>
2020-01-08 19:33:56 +0530

    Started Work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HerosectionComponent } from './Components/herosection/herosection.component';
+import { AboutmeComponent } from './Components/aboutme/aboutme.component';
+import { CopyrightComponent } from './Components/copyright/copyright.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosectionComponent,
+    AboutmeComponent,
+    CopyrightComponent,
+    ContactComponent,
+    GalleryComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
